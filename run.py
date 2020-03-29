6ab8132a72e5264a0b8c0bbe5f7bbd7c57fc67cb
soroosh <soroosh.sarabadani@gmail.com>
2020-03-29 19:52:05 +0200

    run challenge

--- run.py
+++ run.py
@@ -1,5 +1,30 @@
+from typing import Union
+
 import yaml
+import os
+
+
+class Result(object):
+    pass
+
+
+class RunError(object):
+    pass
+
+
+def run_challenge(challenge_name: str, repository: str) -> Union[Result, RunError]:
+    code = os.system('''
+            export REPOSITRY_URL={}
+            export CHALLENGE_NAME={}
+            bash run.sh'''.format(p['repository'], 'prime'))
+    print(code)
+    if str(code) != 0:
+        # TODO: handle error
+        return RunError()
+
 
 if __name__ == '__main__':
     with open('participants.yml') as participants_stream:
         participants = yaml.load(participants_stream, Loader=yaml.FullLoader)
+    for p in participants:
+        result = run_challenge('prime', p['repository'])
