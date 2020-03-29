e5cb44937f28c04ce1dbb066ddd01ffbf36b5bef
soroosh <soroosh.sarabadani@gmail.com>
2020-03-30 00:25:32 +0200

    prime poc

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 set -e
+BASE_PATH=$(pwd)
 rm -rf  workspace
 mkdir workspace && cd workspace
 git clone $REPOSITRY_URL $(pwd)
 cd $CHALLENGE_NAME
 docker build -t $CHALLENGE_NAME .
-docker run $CHALLENGE_NAME
+echo 'Running the test...'
+docker run -v $BASE_PATH/challenges/prime/input.txt:/data/input.txt $CHALLENGE_NAME /data/input.txt  > result.txt
+echo 'Test Done...'
+diff --strip-trailing-cr -w result.txt $BASE_PATH/challenges/prime/expected.txt
+
 
 
