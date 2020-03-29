1e0117c448af16e88eb1f675591a4fb27c94cd65
soroosh <soroosh.sarabadani@gmail.com>
2020-03-29 20:07:01 +0200

    run built image

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 set -e
 rm -rf  workspace
 mkdir workspace && cd workspace
-git clone $REPOSITRY_URL
+git clone $REPOSITRY_URL $(pwd)
 cd $CHALLENGE_NAME
 docker build -t $CHALLENGE_NAME .
+docker run $CHALLENGE_NAME
 
 
