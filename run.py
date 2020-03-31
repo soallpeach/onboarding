94fe6a5cad86707eab49cc377fe29007f0a07e0c
soroosh <soroosh.sarabadani@gmail.com>
2020-03-31 20:01:00 +0200

    use subprocess

--- run.py
+++ run.py
@@ -34,29 +34,47 @@ class ChallengeExecution(object):
         self.repository = repository
         self.challenge_name = challenge_name
 
-    def run_step(self, script_path: str) -> int:
-        code = os.system('''
+    def run_step(self, name: str, script_path: str) -> StepResult:
+        start = time.time()
+
+        p = subprocess.Popen('''
                 export REPOSITORY_URL={}
                 export CHALLENGE_NAME={}
-                bash {}
-                '''.format(self.repository, self.challenge_name, script_path))
+                bash {} 
+                '''.format(self.repository, self.challenge_name, script_path),
+                             shell=True,
+                             stdout=subprocess.PIPE
+                             )
 
-        return int(code)
+        elapsed = time.time()
+        duration = round(elapsed - start, 3)
+
+        p.wait()
+        print(p.poll())
+        stdout_lines = p.stdout.readlines() if p.stdout else ''
+        stderr_lines = p.stdin.readlines() if p.stdin else ''
+
+        stdout = ''.join([line.decode("utf-8") for line in stdout_lines])
+        stderr = ''.join([line.decode("utf-8") for line in stderr_lines])
+
+        return StepResult(name, int(p.poll()), duration, stdout, stderr)
 
 
-def run_challenge(challenge_execution: ChallengeExecution) -> Union[Result, RunError]:
+def run_challenge(challenge_execution: ChallengeExecution) -> Union[ChallengeResult, ChallengeError]:
     print(f"Running challenge ${challenge_execution.challenge_name} from repository ${challenge_execution.repository}",
           flush=True)
-    build_code = challenge_execution.run_step('scripts/build.sh')
-    run_code = challenge_execution.run_step('scripts/run_in_file_program.sh')
-    validate_code = challenge_execution.run_step('scripts/validate.sh')
+    build_result = challenge_execution.run_step('build', 'scripts/build.sh')
+    if build_result.code != 0:
+        return ChallengeError('Error in building the image', build_result)
+    run_result = challenge_execution.run_step('run', 'scripts/run_in_file_program.sh')
+    if run_result.code != 0:
+        return ChallengeError('Error in running the code', run_result)
+    validate_result = challenge_execution.run_step('validate', 'scripts/validate.sh')
 
-    print(build_code)
-    print(run_code)
-    print(validate_code)
-    if build_code != 0:
-        # TODO: handle error
-        return RunError()
+    if validate_result.code != 0:
+        return ChallengeError('Error in validating the result', validate_result)
+
+    return ChallengeResult(build_result, run_result, validate_result)
 
 
 if __name__ == '__main__':
@@ -68,5 +86,5 @@ if __name__ == '__main__':
 
     for challenge in challenges:
         for p in participants:
-            challenge_execution = ChallengeExecution(challenge['name'], p['repository'])
-            result = run_challenge(challenge_execution)
+            ce = ChallengeExecution(challenge['name'], p['repository'])
+            result = run_challenge(ce)
