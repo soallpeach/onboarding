df7d078164b985cafb7386d0ca6cee7a2734f92a
soroosh <soroosh.sarabadani@gmail.com>
2020-04-04 14:08:23 +0200

    extract models

---
+++ models.py
@@ -0,0 +1,31 @@
+class StepResult(object):
+    def __init__(self, name: str, code: int, duration: int,
+                 stdout: str, stderr: str):
+        self.name = name
+        self.code = code
+        self.duration = duration
+        self.stderr = stderr
+        self.stdout = stdout
+
+    def __repr__(self) -> str:
+        return self.name + " " + str(self.code) + " " + str(self.duration) + " " + self.stdout + " " + self.stderr
+
+
+class ChallengeResult(object):
+    validate_result: StepResult
+    run_result: StepResult
+    build_result: StepResult
+
+    def __init__(self, build_result: StepResult, run_result: StepResult, validate_result: StepResult):
+        self.build_result = build_result
+        self.run_result = run_result
+        self.validate_result = validate_result
+
+    def __str__(self) -> str:
+        return str(self.__dict__)
+
+
+class ChallengeError(Exception):
+    def __init__(self, message: str, step_result: StepResult):
+        self.message = message
+        self.step_result = step_result
--- reporter.py
+++ reporter.py
@@ -4,14 +4,14 @@ from requests import session, Session
 
 import os
 
-from run import ChallengeError, ChallengeResult
+from models import ChallengeResult, ChallengeError
 
 BASE_URL = os.getenv('API_URL', 'https://soallpeach-api-soroosh.fandogh.cloud')
 
 
 def get_session() -> Session:
     session.headers.update({
-        'Authorization': 'TOEKN ' + os.getenv('API_SECRET_KEY')
+        'Authorization': 'TOEKN ' + os.getenv('API_SECRET_KEY', 'STRONG_TOKEN')
     })
     return session
 
