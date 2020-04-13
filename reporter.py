d730611c97871799aeb2ddbdb2fff2ce77198d44
soroosh <soroosh.sarabadani@gmail.com>
2020-04-13 12:49:10 +0200

    add round functions

--- reporter.py
+++ reporter.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from requests import session, Session
+from requests import session, Session, Response
 import json
 
 import os
@@ -35,9 +35,30 @@ class ReportRequest(object):
         self.state = 'PASSED' if isinstance(result, ChallengeResult) else 'FAILED'
 
 
+def is_ok(status_code: int) -> bool:
+    if 200 <= status_code <= 299:
+        return True
+    return False
+
+
+def raise_error_on_not_ok(response: Response, message: str):
+    if not is_ok(response.status_code):
+        raise Exception(f'{message}, response: {response.text} status:{response.status_code}')
+
+
 def report(nickname: str, challenge_name: str, run_id: str, result: Union[ChallengeResult, ChallengeError]):
     request = ReportRequest(nickname, challenge_name, run_id, result)
     request_json = json.dumps(request.__dict__, default=lambda o: o.__dict__, indent=4)
     response = get_session().post(f'{BASE_URL}/scores', data=request_json)
-    print(response)
-    print(response.text)
+    raise_error_on_not_ok(response, 'Error in reporting')
+
+
+def start_round(round_id: int, challenge_name: str):
+    response = get_session().post(f'{BASE_URL}/challenges/{challenge_name}/rounds',
+                                  json={'round_id': round_id, 'challenge_name': challenge_name})
+    raise_error_on_not_ok(response, f'Error in starting round {round_id} chalenge {challenge_name}')
+
+
+def finish_round(round_id: int, challenge_name: str):
+    response = get_session().patch(f'{BASE_URL}/challenges/{challenge_name}/rounds{round_id}', json={'status': 'DONE'})
+    raise_error_on_not_ok(response, f'Error in finishing round {round_id} challenge {challenge_name}')
