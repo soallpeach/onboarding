16d139b3214e124eea087866aad8a7315568ec08
soroosh <soroosh.sarabadani@gmail.com>
2020-04-15 18:24:59 +0200

    redirect to a file inside container instead of out of it.

--- scripts/run_in_file_program.sh
+++ scripts/run_in_file_program.sh
@@ -1,12 +1,10 @@
-set -eu
+set -eux
 BASE_PATH=$(pwd)
-
 cd workspace/$CHALLENGE_NAME
-rm -rf data/
 mkdir data/
-cp $BASE_PATH/challenges/$CHALLENGE_NAME/input.txt data/in.txt
-docker run --rm -v "$PWD/data/:/data/" $CHALLENGE_NAME /data/in.txt /data/out.txt > result.txt
-# Make sure old programs that use stdout and new programs using output file works.
-if [[ ! -s result.txt && -s data/out.txt ]]; then
-  mv data/out.txt result.txt;
-fi
+cp $BASE_PATH/challenges/$CHALLENGE_NAME/input.txt data/input.txt
+COMMAND=$(docker inspect prime --format='{{.ContainerConfig.Entrypoint}}')
+CMD=${COMMAND:1:$((${#COMMAND} - 2))} #remove first and last char (long version to work on both Linux and Mac)
+docker run --rm -v "$PWD/data/:/data/" --entrypoint="" $CHALLENGE_NAME  sh -c "$CMD /data/input.txt > /data/result.txt"
+
+
--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,6 +1,6 @@
 set -e
 BASE_PATH=$(pwd)
-cd workspace/$CHALLENGE_NAME
+cd workspace/$CHALLENGE_NAME/data
 diff --strip-trailing-cr -w result.txt $BASE_PATH/challenges/$CHALLENGE_NAME/expected.txt
 
 
