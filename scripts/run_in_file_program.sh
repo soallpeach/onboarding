e24d472190c8fe3c1ec904b28245e10e3c0627bd
liamplm <liamplm82@gmail.com>
2020-04-15 12:51:20 +0430

    merge master

--- scripts/run_in_file_program.sh
+++ scripts/run_in_file_program.sh
@@ -1,4 +1,5 @@
 set -e
 BASE_PATH=$(pwd)
 cd workspace/$CHALLENGE_NAME
-docker run -v $BASE_PATH/challenges/$CHALLENGE_NAME/input.txt:/data/input.txt $CHALLENGE_NAME /data/input.txt  > result.txt
+docker run --rm -v $BASE_PATH/challenges/$CHALLENGE_NAME/input.txt:/data/input.txt $CHALLENGE_NAME /data/input.txt  > result.txt
+
