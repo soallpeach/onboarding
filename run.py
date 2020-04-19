1f8222e7e53e3ab811c79e294fc909f4d306750d
soroosh <soroosh.sarabadani@gmail.com>
2020-04-19 17:39:47 +0200

    update run.py to support custom scripts

--- run.py
+++ run.py
@@ -5,21 +5,26 @@ import yaml
 import subprocess
 import reporter
 import time
+import shutil
 
-from models import StepResult, ChallengeResult, ChallengeError
+from models import StepResult, ChallengeResult, ChallengeError, Challenge
 
 
 class ChallengeExecution(object):
-    def __init__(self, challenge_name: str, repository: str):
+    def __init__(self, challenge: Challenge, repository: str):
+        self.challenge = challenge
         self.repository = repository
-        self.challenge_name = challenge_name
 
-    def run_step(self, name: str, script_path: str, timeout: int = 10 * 60,
+    def prepare_workspace(self):
+        shutil.rmtree('./workspace', ignore_errors=True)
+        shutil.copytree(f'./challenges/{self.challenge.name}', './workspace/')
+
+    def run_step(self, cmd: str, name: str, script_path: str, timeout: int = 10 * 60,
                  get_durtion_from_stdout: bool = False) -> StepResult:
         start = time.time()
-        script_envs = {**os.environ, 'CHALLENGE_NAME': self.challenge_name, 'REPOSITORY_URL': self.repository}
+        script_envs = {**os.environ, 'CHALLENGE_NAME': self.challenge.name, 'REPOSITORY_URL': self.repository}
 
-        p = subprocess.Popen('bash {} '.format(script_path),
+        p = subprocess.Popen(f'{cmd} {script_path}',
                              shell=True,
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE,
@@ -29,7 +34,14 @@ class ChallengeExecution(object):
         try:
             p.wait(timeout)
         except subprocess.TimeoutExpired as e:
-            return StepResult(name, 9999, timeout, '', 'Timeout')
+            stdout_lines = p.stdout.readlines() if p.stdout else ''
+            stderr_lines = p.stderr.readlines() if p.stderr else ''
+            stdout = ''.join([line.decode("utf-8") for line in stdout_lines])
+            stderr = ''.join([line.decode("utf-8") for line in stderr_lines])
+
+            print(stderr)
+            print(stdout)
+            return StepResult(name, 9999, timeout, stdout, stderr + '\nTimeout')
 
         stdout_lines = p.stdout.readlines() if p.stdout else ''
         stderr_lines = p.stderr.readlines() if p.stderr else ''
@@ -55,22 +67,45 @@ class ChallengeExecution(object):
         return StepResult(name, int(p.poll()), duration, stdout, stderr)
 
 
+run_scripts = {
+    'file': 'run_in_file_program.sh',
+    'http': 'run_in_http_program.sh'
+}
+
+
 def run_challenge(challenge_execution: ChallengeExecution) -> Union[ChallengeResult, ChallengeError]:
-    print(f"Running challenge {challenge_execution.challenge_name} from repository {challenge_execution.repository}",
+    print(f"Running challenge {challenge_execution.challenge.name} from repository {challenge_execution.repository}",
           flush=True)
-    build_result = challenge_execution.run_step('build', 'scripts/build.sh')
+
+    challenge_execution.prepare_workspace()
+    build_result = challenge_execution.run_step('bash', 'build', 'scripts/build.sh')
     if build_result.code != 0:
         return ChallengeError('Error in building the image', build_result)
-    run_result = challenge_execution.run_step('run', 'scripts/run_in_file_program.sh', timeout=20,
-                                              get_durtion_from_stdout=True)
-    if run_result.code != 0:
-        return ChallengeError('Error in running the code', run_result)
-    validate_result = challenge_execution.run_step('validate', 'scripts/validate.sh', timeout=10)
-    challenge_execution.run_step('cleanup', 'scripts/cleanup.sh')
 
-    if validate_result.code != 0:
-        return ChallengeError('Error in validating the result', validate_result)
+    get_duration_from_stdout = True if challenge_execution.challenge.input_model == 'file' else False
+    if challenge_execution.challenge.custom_runner:
+        run_result = challenge_execution.run_step('bash', 'run', f'./workspace/run.sh', timeout=10,
+                                                  get_durtion_from_stdout=get_duration_from_stdout)
+        if run_result.code != 0:
+            return ChallengeError('Error in running the code', run_result)
 
+        validate_result = challenge_execution.run_step('python', 'validate', f'./workspace/validate.py', timeout=10)
+
+        if validate_result.code != 0:
+            return ChallengeError('Error in validating the result', validate_result)
+
+    else:
+        run_script = run_scripts.get(challenge_execution.challenge.input_model)
+        run_result = challenge_execution.run_step('bash', 'run', f'scripts/{run_script}', timeout=10,
+                                                  get_durtion_from_stdout=get_duration_from_stdout)
+        if run_result.code != 0:
+            return ChallengeError('Error in running the code', run_result)
+        validate_result = challenge_execution.run_step('bash', 'validate', 'scripts/validate.sh', timeout=10)
+
+        if validate_result.code != 0:
+            return ChallengeError('Error in validating the result', validate_result)
+
+    challenge_execution.run_step('bash', 'cleanup', 'scripts/cleanup.sh')
     return ChallengeResult(build_result, run_result, validate_result)
 
 
@@ -82,13 +117,16 @@ if __name__ == '__main__':
         challenges = yaml.load(challenges_stream, Loader=yaml.FullLoader)
 
     run_id = int(time.time())
-    for challenge in challenges:
-        challenge_name = challenge['name']
+    for challenge_dict in challenges:
+        challenge_name = challenge_dict['name']
+        challenge_input_model = challenge_dict['input_model']
+        custom_runner = challenge_dict['custom_runner']
+        challenge = Challenge(challenge_name, '', challenge_input_model, custom_runner)
         reporter.start_round(run_id, challenge_name)
         for p in participants:
             repository = p['repository']
             nickname = p['nickname']
-            ce = ChallengeExecution(challenge_name, p['repository'])
+            ce = ChallengeExecution(challenge, p['repository'])
             result = run_challenge(ce)
             reporter.report(nickname, challenge_name, run_id, result)
             print(result)
