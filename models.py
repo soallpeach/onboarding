2864aec7f8d15cba63534cfe8e1348efc36a2080
soroosh <soroosh.sarabadani@gmail.com>
2020-04-20 00:12:09 +0200

    use dataclass in models

--- models.py
+++ models.py
@@ -1,39 +1,31 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Challenge(object):
-    def __init__(self, name: str, description: str, input_model: str, custom_runner: bool):
-        self.name = name
-        self.description = description
-        self.input_model = input_model
-        self.custom_runner = custom_runner
+    name: str
+    description: str
+    input_model: str
+    custom_runner: bool
 
 
+@dataclass
 class StepResult(object):
-    def __init__(self, name: str, code: int, duration: int,
-                 stdout: str, stderr: str):
-        self.name = name
-        self.code = code
-        self.duration = duration
-        self.stderr = stderr
-        self.stdout = stdout
-
-    def __repr__(self) -> str:
-        return self.name + " " + str(self.code) + " " + str(self.duration) + " " + self.stdout + " " + self.stderr
+    name: str
+    code: int
+    duration: int
+    stdout: str
+    stderr: str
 
 
+@dataclass
 class ChallengeResult(object):
     validate_result: StepResult
     run_result: StepResult
     build_result: StepResult
 
-    def __init__(self, build_result: StepResult, run_result: StepResult, validate_result: StepResult):
-        self.build_result = build_result
-        self.run_result = run_result
-        self.validate_result = validate_result
 
-    def __str__(self) -> str:
-        return str(self.__dict__)
-
-
+@dataclass
 class ChallengeError(Exception):
-    def __init__(self, message: str, step_result: StepResult):
-        self.message = message
-        self.step_result = step_result
+    message: str
+    step_result: StepResult
