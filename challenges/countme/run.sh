4bccbaf7dff0fb0502bf1afcaa37711e05d2bb33
soroosh <soroosh.sarabadani@gmail.com>
2020-04-26 00:31:31 +0200

    countme run step

--- challenges/countme/run.sh
+++ challenges/countme/run.sh
@@ -1,14 +1,10 @@
 set -eu
 
-DATE_COMMAND="date"
-
-if [[ $(uname -s) == "Darwin" ]]; then
-  DATE_COMMAND="gdate"
-fi
-BASE_PATH=$(pwd)
+SEED=$(echo $((1 + RANDOM % 10)))
+BASE_PATH=$(pwd)/workspace
 cd workspace/code/$CHALLENGE_NAME
+echo $SEED > payload.txt
 mkdir data/
-cp $BASE_PATH/challenges/$CHALLENGE_NAME/input.txt data/input.txt
 
 set +e
 docker container rm "$CHALLENGE_NAME-container" -f &> /dev/null
@@ -16,7 +12,9 @@ set -eu
 
 docker run -d --name "$CHALLENGE_NAME-container" -p "8080:80" $CHALLENGE_NAME
 
-echo "GET http://localhost:8080" | vegeta  -cpus 1 attack -rate 100   -duration=2s  | vegeta report -type=json | jq '.' > metrics.json
+timeout 60 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8080/count)" != "200" ]]; do sleep 1; done' || false
+
+vegeta  -cpus 1 attack -rate $rate   -duration=${duration}s  -targets $BASE_PATH/target.list | vegeta report -type=json | jq '.' > metrics.json
 cat metrics.json
 echo "::::METRICS="$(cat metrics.json)
 
